c7184ef2541efa38fd2c1ef89395aadbe64e15ea
Angel Casado <acasadoalonso@gmail.com>
2019-03-20 13:50:13 +0100

    add the delete stations script

---
+++ deletestations.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+bash deletephantoms.sh NAVITER
+bash deletephantoms.sh FLYMASTER
+bash deletephantoms.sh SPOT
+bash deletephantoms.sh SPIDER
+bash deletephantoms.sh INREACH
+bash deletephantoms.sh SKYLINES
+bash deletephantoms.sh LT24
+bash deletephantoms.sh CAPTURS
+bash deleteFNB.sh      FNB
+bash deleteFNB.sh      XCG
