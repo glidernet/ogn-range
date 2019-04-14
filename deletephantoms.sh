0fd89ee5d92e0dbe459f1509ca59389511f92528
Angel Casado <acasadoalonso@gmail.com>
2019-04-14 11:04:17 +0200

    fix deleteFNB script

--- deletephantoms.sh
+++ deletephantoms.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 echo "select * from stations                where  station = '$1';"       
-echo "select * from stations                where  station = '$1';"                                                | mysql --login-path=ognrange ognrange 2>/dev/null
-echo "delete   from stationlocation         where  station     = (select id from stations where station = '$1'); " | mysql --login-path=ognrange ognrange 2>/dev/null
-echo "delete   from roughcoverage           where  station     = (select id from stations where station = '$1'); " | mysql --login-path=ognrang  ognrange 2>/dev/null
-echo "delete   from availability            where  station_id  = (select id from stations where station = '$1'); " | mysql --login-path=ognrange ognrange 2>/dev/null
-echo "delete   from availability_log        where  station_id  = (select id from stations where station = '$1'); " | mysql --login-path=ognrange ognrange 2>/dev/null
-echo "delete   from positions_mgrs          where  station     = (select id from stations where station = '$1'); " | mysql --login-path=ognrange ognrange 2>/dev/null
-echo "delete   from stats                   where  station     = (select id from stations where station = '$1'); " | mysql --login-path=ognrange ognrange 2>/dev/null
-echo "delete   from statssummary            where  station     = (select id from stations where station = '$1'); " | mysql --login-path=ognrange ognrange 2>/dev/null
-echo "delete   from stations                where  station = '$1'; "                                               | mysql --login-path=ognrange ognrange 2>/dev/null
+mysql --login-path=ognrange -e "select * from stations                where  station = '$1';"                                                ognrange
+mysql --login-path=ognrange -e "delete   from stationlocation         where  station     = (select id from stations where station = '$1'); " ognrange 
+mysql --login-path=ognrange -e "delete   from roughcoverage           where  station     = (select id from stations where station = '$1'); " ognrange
+mysql --login-path=ognrange -e "delete   from availability            where  station_id  = (select id from stations where station = '$1'); " ognrange
+mysql --login-path=ognrange -e "delete   from availability_log        where  station_id  = (select id from stations where station = '$1'); " ognrange
+mysql --login-path=ognrange -e "delete   from positions_mgrs          where  station     = (select id from stations where station = '$1'); " ognrange
+mysql --login-path=ognrange -e "delete   from stats                   where  station     = (select id from stations where station = '$1'); " ognrange
+mysql --login-path=ognrange -e "delete   from statssummary            where  station     = (select id from stations where station = '$1'); " ognrange
+mysql --login-path=ognrange -e "delete   from stations                where  station = '$1'; "                                               ognrange
