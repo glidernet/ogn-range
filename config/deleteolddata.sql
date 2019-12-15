14bc9892b649429640b302be3268360ec5bf42bf
Angel Casado <acasadoalonso@gmail.com>
2019-12-15 20:10:41 +0100

    sync

--- config/deleteolddata.sql
+++ config/deleteolddata.sql
@@ -1,13 +1,16 @@
 use ognrange;
-SELECT count(*) FROM `availability_log` WHERE time < unix_timestamp('2018-10-01');
-DELETE FROM `availability_log` WHERE time < unix_timestamp('2018-10-01');
+set innodb_lock_wait_timeout=100;
+SELECT 'availlog:', count(*) FROM `availability_log` WHERE time < unix_timestamp('2019-01-01');
+DELETE FROM `availability_log` WHERE time < unix_timestamp('2019-01-01');
 optimize table availability_log;
-SELECT count(*) FROM `positions_mgrs` WHERE `time` < '2018-10-01';
-DELETE FROM `positions_mgrs` WHERE `time` < '2018-10-01';
-optimize table positions_mgrs;
-SELECT count(*) FROM `history` WHERE time <'2018-10-01';
-DELETE FROM `history` WHERE time <'2018-10-01';
+SELECT 'History:', count(*) FROM `history` WHERE time <'2019-01-01';
+DELETE FROM `history` WHERE time <'2019-01-01';
 optimize table history;
-SELECT count(*) FROM `stats` WHERE time <'2018-10-01';
-DELETE FROM `stats` WHERE time <'2018-10-01';
+SELECT 'Stats:', count(*) FROM `stats` WHERE time <'2019-01-01';
+DELETE FROM `stats` WHERE time <'2019-01-01';
 optimize table stats;
+SELECT 'PosMgrs:', count(*) FROM `positions_mgrs` WHERE `time` < '2019-01-01';
+DELETE FROM `positions_mgrs` WHERE `time` < '2019-01-01';
+optimize table positions_mgrs;
+
+
