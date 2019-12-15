c4a6ab396542a9c5a6326638e29a4bc8b0f44ea1
Angel Casado <acasadoalonso@gmail.com>
2019-12-15 20:06:21 +0100

    Sync

--- config/deleteolddata.sql
+++ config/deleteolddata.sql
@@ -1,9 +1,13 @@
-
-SELECT count(*) FROM `availability_log` WHERE time < unix_timestamp('2018-01-01');
-DELETE FROM `availability_log` WHERE time < unix_timestamp('2018-01-01');
-SELECT count(*) FROM `positions_mgrs` WHERE `time` < '2018-01-01';
-DELETE FROM `positions_mgrs` WHERE `time` < '2018-01-01';
-SELECT count(*) FROM `history` WHERE time <'2018-01-01';
-DELETE FROM `history` WHERE time <'2018-01-01';
-SELECT count(*) FROM `stats` WHERE time <'2018-01-01';
-DELETE FROM `stats` WHERE time <'2018-01-01';
+use ognrange;
+SELECT count(*) FROM `availability_log` WHERE time < unix_timestamp('2018-10-01');
+DELETE FROM `availability_log` WHERE time < unix_timestamp('2018-10-01');
+optimize table availability_log;
+SELECT count(*) FROM `positions_mgrs` WHERE `time` < '2018-10-01';
+DELETE FROM `positions_mgrs` WHERE `time` < '2018-10-01';
+optimize table positions_mgrs;
+SELECT count(*) FROM `history` WHERE time <'2018-10-01';
+DELETE FROM `history` WHERE time <'2018-10-01';
+optimize table history;
+SELECT count(*) FROM `stats` WHERE time <'2018-10-01';
+DELETE FROM `stats` WHERE time <'2018-10-01';
+optimize table stats;
