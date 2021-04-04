163721d87bef85bab34fe9506a655f946544332e
Angel Casado <acasadoalonso@gmail.com>
2021-04-04 19:18:23 +0200

    create sh dir and sync some files ...

---
+++ sh/OGNrangecheck.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+pnum=$(pgrep -x -f "perl fetchrange3.pl")
+if [ $? -eq 0 ] # if OGN repo interface is  not running
+then
+    logger -t $0 "OGNrange Log is alive"
+else
+	#               restart OGN data collector
+    logger -t $0 "OGNrange Log seems down, restarting"
+    date >>/nfs/OGN/DIRdata/log/.OGNrangerestart.log
+    bash ~/src/sh/OGNrange.sh &
+fi
+
---
+++ sh/OGNrangekillandgo.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+killall perl
+bash /home/angel/src/sh/OGNrange.sh &
