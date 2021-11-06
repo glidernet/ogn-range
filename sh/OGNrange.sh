d90845854f90874992c8b3e723ff394938eaabd8
Angel Casado <acasadoalonso@gmail.com>
2021-11-06 12:21:48 +0100

    Some adjustments

--- sh/OGNrange.sh
+++ sh/OGNrange.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd /var/www/html/OGNRANGE/bin
+mysql -e "delete from stations where otime = '1970-01-01';" ognrange
 echo $$                                                     >/tmp/OGNrange$(date +%y%m%d-%H-%M-%S).log
 echo $(date +%H:%M:%S)                                     >>/nfs/OGN/DIRdata/log/OGNrange$(date +%y%m%d).log
 echo "============="                                       >>/nfs/OGN/DIRdata/log/OGNrange$(date +%y%m%d).log
